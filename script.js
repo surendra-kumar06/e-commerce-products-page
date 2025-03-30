6cb96cdf42166002e2848eacb65c0ba252b35696
surendra kumar <surendra@gmail.com>
2025-03-30 16:05:28 +0530

    final touch

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ navSlider.addEventListener('click',()=>{
 // Product display section ----------------
 const baseUrl = "https://fakestoreapi.in/api/products";
 
-// categories
+// get categories
 const category = document.querySelector('#category');
 ( async ()=>{
     const res = await fetch(`${baseUrl}/category`);
@@ -38,23 +38,42 @@ const category = document.querySelector('#category');
     }).join(' ');
 })();
 
-// range input box function
+// set range of input box function
 const priceRange = document.querySelector('.setPriceRange');
 const priceRangeInput = document.querySelector('#price-rang-input');
 priceRangeInput.addEventListener('input',(e)=>{
     priceRange.innerText = e.target.value;
 });
 
+// function for toast
+const toast = document.getElementById("toast");
+const toastMessage = document.querySelector('.toast-message');
+const showToast = (message)=>{
+    toastMessage.innerText = message;
+    toast.classList.toggle('show');
+    setTimeout(()=>{
+        toast.classList.toggle('show');
+    },2000)
+}
+
+// functions for add to cart
+let cartItems = 0;
+const cartCount = document.querySelector('.cart-count');
+const addToCard = ()=>{
+    showToast('Item added in cart successfully');
+    cartItems += 1;
+    cartCount.innerText = cartItems;
+}
 
 
 // set products function
+let products = document.querySelector(".products");
 const setProducts = (data)=>{
-    let products = document.querySelector(".products");
-    products.innerHTML = data.map((item)=>{
-        return `
+    data.map((item)=>{
+        let htmlData =  `
              <div class="card" id='${item.id}'>
             <div class="card-img">
-              <img src="${item.image}" alt="product-image">
+              <img src="${item.image}" loading="lazy" alt="product-image">
             </div>
             <div class="card-details">
               <h3 class="card-title">${item.title.split(' ').length > 5 ? item.title.split(' ').slice(0,5).join(' ')+'...' : item.title}</h3>
@@ -65,34 +84,78 @@ const setProducts = (data)=>{
               </div>
             </div>
            <div class="card-btns">
-            <button class="addToCard-btn">
+            <button class="addToCard-btn" onclick="addToCard()">
               <i class="fa-solid fa-cart-shopping"></i> ADD TO CARD</button>
             <button class="buyNow-btn"><i class="fa-solid fa-bolt"></i> BUY NOW</button>
            </div>
           </div>
     `
-    }).join(" ")
+    products.insertAdjacentHTML('beforeend',htmlData)
+    });
     
 }
 
 
-
+let limit = 6;
+let page = 1;
+let param = '?';
+let paginationQuery = `page=${page}&limit=${limit}`;
+let query = ()=>{
+    return param+paginationQuery;
+}
+const updateQuery = ()=>{
+    paginationQuery = `page=${page}&limit=${limit}`;
+}
 const getProducts = async ()=>{
-    const response = await fetch(`${baseUrl}?offset=0&limit=6`);
+    updateQuery();
+    const response = await fetch(baseUrl+query());
     const data = await response.json();
-    console.log();
     setProducts(data.products);
-    
 }
+getProducts();
 
-getProducts()
+window.addEventListener('scroll',()=>{
+    let {scrollTop, clientHeight, scrollHeight} = document.documentElement;
+    if(scrollTop + clientHeight+1 >= scrollHeight){
+        page += 1;
+        getProducts(); 
+    }
+})
 
-// Apply button function
-// const applyBtn = document.querySelector('.apply-btn');
-// applyBtn.addEventListener('click',()=>{
-//     if(category.value){
-//         getProducts(`/category/${category.value}`)
-//     }else{
-//         getProducts()
-//     }
-// })
+
+// function for apply Button
+const applyBtn = document.querySelector('.apply-btn');
+applyBtn.addEventListener('click',()=>{
+    if(category.value){
+        products.innerHTML = '';
+        page=1;
+        param = `/category?type=${category.value}&`;
+        getProducts();
+    }else{
+        param='?';
+        getProducts();
+    }
+})
+
+// function for search button
+const searchBtn = document.querySelector(".search-btn");
+const searchInput = document.getElementById('searchInput');
+searchBtn.addEventListener('click',()=>{
+    if(searchInput.value){
+        products.innerHTML = "";
+        page =1;
+        param = `?search=${searchInput.value}`;
+        getProducts();
+    }
+})
+
+
+// Back to Top
+const backToTopBtn = document.querySelector('.backTopBtn');
+window.onscroll = function() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        backToTopBtn.style.display = "flex"; 
+    } else {
+        backToTopBtn.style.display = "none"; 
+    }
+};
